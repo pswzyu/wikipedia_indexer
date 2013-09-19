62e541d351ea40b0bb953c16618a8bde164055e6
xcv58 <chenyihonglove@gmail.com>
2013-09-19 14:56:20 -0400

    TokenRules Accent and Stopwords

--- src/edu/buffalo/cse/ir/wikiindexer/tokenizer/rules/AccentsDefault.java
+++ src/edu/buffalo/cse/ir/wikiindexer/tokenizer/rules/AccentsDefault.java
@@ -1,5 +1,29 @@
 package edu.buffalo.cse.ir.wikiindexer.tokenizer.rules;
 
-public class AccentsDefault {
+import java.text.Normalizer;
+import java.text.Normalizer.Form;
+import java.util.Arrays;
+import java.util.HashSet;
+
+import edu.buffalo.cse.ir.wikiindexer.tokenizer.TokenStream;
+import edu.buffalo.cse.ir.wikiindexer.tokenizer.TokenizerException;
+import edu.buffalo.cse.ir.wikiindexer.tokenizer.rules.TokenizerRule.RULENAMES;
+
+
+@RuleClass(className = RULENAMES.ACCENTS)
+public class AccentsDefault implements TokenizerRule {
+
+	public void apply(TokenStream stream) throws TokenizerException {
+		stream.reset();
+		
+		while (stream.hasNext()) {
+			String token = stream.next();
+			String tmp = Normalizer.normalize(token, Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}+","");
+			if(!token.equals(tmp)) {
+				stream.set(tmp);
+			}
+		}
+	}
+
 
 }
